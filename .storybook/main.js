cd23e5a1dcd8eab0b813a0b1809e26908c70f2f0
itzikd <itzikd@netapp.com>
2023-02-26 16:38:31 +0200

    test

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../projects/my-lib/src/**/*.stories.mdx",
+    "../projects/my-lib/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/angular",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
