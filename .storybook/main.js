af2eff7060757395f8d7ad3dac5942ce59af1805
itzikd <itzikd@netapp.com>
2023-03-09 13:12:51 +0200

    packages update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,15 @@
 module.exports = {
-  stories: ["../projects/my-lib/src/**/*.mdx", "../projects/my-lib/src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
-  framework: {
-    name: "@storybook/angular",
-    options: {}
-  },
-  docs: {
-    autodocs: true
-  },
-    core: {
+    stories:   ["../projects/my-lib/src/**/*.mdx", "../projects/my-lib/src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons:    ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions",
+        "@storybook/addon-mdx-gfm", "storybook-addon-designs"],
+    framework: {
+        name:    "@storybook/angular",
+        options: {}
+    },
+    docs:      {
+        autodocs: true
+    },
+    core:      {
         builder: "webpack5",
     },
 };
